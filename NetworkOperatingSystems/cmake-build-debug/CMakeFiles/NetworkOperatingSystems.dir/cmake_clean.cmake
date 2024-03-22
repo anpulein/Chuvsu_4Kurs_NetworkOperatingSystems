8ad3855ca89414956c6a10786fc5b5460ebb8c97
anpulein <anpuleinWork@gmail.com>
2024-03-22 08:37:51 +0300

    del cashed

---
+++ NetworkOperatingSystems/cmake-build-debug/CMakeFiles/NetworkOperatingSystems.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetworkOperatingSystems.dir/Lab1/model_thread.c.o"
+  "CMakeFiles/NetworkOperatingSystems.dir/Lab1/model_thread.c.o.d"
+  "CMakeFiles/NetworkOperatingSystems.dir/Lab2/model_process.c.o"
+  "CMakeFiles/NetworkOperatingSystems.dir/Lab2/model_process.c.o.d"
+  "CMakeFiles/NetworkOperatingSystems.dir/main.c.o"
+  "CMakeFiles/NetworkOperatingSystems.dir/main.c.o.d"
+  "NetworkOperatingSystems"
+  "NetworkOperatingSystems.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NetworkOperatingSystems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ NetworkOperatingSystems/cmake-build-debug/CMakeFiles/NetworkOperatingSystems.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for NetworkOperatingSystems.
+# This may be replaced when dependencies are built.
